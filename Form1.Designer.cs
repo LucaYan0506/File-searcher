561783ba8f21b93e86f922c1cbeec16cfebe9368
Luca Yan <lucayan0506>
2022-03-22 23:52:44 +0000

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace File_searcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.directoryLbl = new System.Windows.Forms.Label();
+            this.directoryCombobox = new System.Windows.Forms.ComboBox();
+            this.open_folder_browserBtn = new System.Windows.Forms.Button();
+            this.file_nameLbl = new System.Windows.Forms.Label();
+            this.file_nameTxt = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.searching_progressLbl = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.files_ListBox = new System.Windows.Forms.ListBox();
+            this.Press_esc_timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // directoryLbl
+            // 
+            this.directoryLbl.AutoSize = true;
+            this.directoryLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.directoryLbl.Location = new System.Drawing.Point(12, 9);
+            this.directoryLbl.Name = "directoryLbl";
+            this.directoryLbl.Size = new System.Drawing.Size(260, 29);
+            this.directoryLbl.TabIndex = 0;
+            this.directoryLbl.Text = "Directory/Folder path";
+            // 
+            // directoryCombobox
+            // 
+            this.directoryCombobox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.directoryCombobox.FormattingEnabled = true;
+            this.directoryCombobox.Location = new System.Drawing.Point(12, 41);
+            this.directoryCombobox.Name = "directoryCombobox";
+            this.directoryCombobox.Size = new System.Drawing.Size(345, 33);
+            this.directoryCombobox.TabIndex = 1;
+            this.directoryCombobox.SelectedIndexChanged += new System.EventHandler(this.directoryCombobox_SelectedIndexChanged);
+            // 
+            // open_folder_browserBtn
+            // 
+            this.open_folder_browserBtn.BackColor = System.Drawing.SystemColors.Control;
+            this.open_folder_browserBtn.BackgroundImage = global::File_searcher.Properties.Resources._3_dots;
+            this.open_folder_browserBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.open_folder_browserBtn.Location = new System.Drawing.Point(364, 41);
+            this.open_folder_browserBtn.Name = "open_folder_browserBtn";
+            this.open_folder_browserBtn.Size = new System.Drawing.Size(53, 33);
+            this.open_folder_browserBtn.TabIndex = 2;
+            this.open_folder_browserBtn.UseVisualStyleBackColor = false;
+            this.open_folder_browserBtn.Click += new System.EventHandler(this.open_folder_browserBtn_Click);
+            // 
+            // file_nameLbl
+            // 
+            this.file_nameLbl.AutoSize = true;
+            this.file_nameLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.file_nameLbl.Location = new System.Drawing.Point(7, 80);
+            this.file_nameLbl.Name = "file_nameLbl";
+            this.file_nameLbl.Size = new System.Drawing.Size(129, 29);
+            this.file_nameLbl.TabIndex = 0;
+            this.file_nameLbl.Text = "File name";
+            // 
+            // file_nameTxt
+            // 
+            this.file_nameTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.file_nameTxt.Location = new System.Drawing.Point(12, 112);
+            this.file_nameTxt.Name = "file_nameTxt";
+            this.file_nameTxt.Size = new System.Drawing.Size(324, 31);
+            this.file_nameTxt.TabIndex = 3;
+            this.file_nameTxt.KeyDown += new System.Windows.Forms.KeyEventHandler(this.file_nameTxt_KeyDown);
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchBtn.Location = new System.Drawing.Point(342, 90);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(75, 53);
+            this.searchBtn.TabIndex = 4;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // searching_progressLbl
+            // 
+            this.searching_progressLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searching_progressLbl.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.searching_progressLbl.Location = new System.Drawing.Point(12, 159);
+            this.searching_progressLbl.Name = "searching_progressLbl";
+            this.searching_progressLbl.Size = new System.Drawing.Size(405, 35);
+            this.searching_progressLbl.TabIndex = 5;
+            this.searching_progressLbl.Text = "Progress";
+            this.searching_progressLbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 187);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(405, 32);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // files_ListBox
+            // 
+            this.files_ListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.files_ListBox.FormattingEnabled = true;
+            this.files_ListBox.HorizontalScrollbar = true;
+            this.files_ListBox.ItemHeight = 20;
+            this.files_ListBox.Location = new System.Drawing.Point(17, 234);
+            this.files_ListBox.Name = "files_ListBox";
+            this.files_ListBox.Size = new System.Drawing.Size(400, 264);
+            this.files_ListBox.TabIndex = 7;
+            this.files_ListBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.files_ListBox_MouseDoubleClick);
+            // 
+            // Press_esc_timer
+            // 
+            this.Press_esc_timer.Interval = 1000;
+            this.Press_esc_timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 509);
+            this.Controls.Add(this.files_ListBox);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.searching_progressLbl);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.file_nameTxt);
+            this.Controls.Add(this.open_folder_browserBtn);
+            this.Controls.Add(this.directoryCombobox);
+            this.Controls.Add(this.file_nameLbl);
+            this.Controls.Add(this.directoryLbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label directoryLbl;
+        private System.Windows.Forms.ComboBox directoryCombobox;
+        private System.Windows.Forms.Button open_folder_browserBtn;
+        private System.Windows.Forms.Label file_nameLbl;
+        private System.Windows.Forms.TextBox file_nameTxt;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.Label searching_progressLbl;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListBox files_ListBox;
+        private System.Windows.Forms.Timer Press_esc_timer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace File_searcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /*                                   Global variables                            */
+
+        //here we save the path of all files shows in the listbox
+        List<string> file_list = new List<string>();
+
+
+        //////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////
+        /*                                 Functions/subroutines                            */
+        private List<DirectoryInfo> get_all_directories(DirectoryInfo directory)
+        {
+            List<DirectoryInfo> all_directories = new List<DirectoryInfo>();
+
+            //from directory get all sub directory
+            foreach(DirectoryInfo sub_directory in directory.GetDirectories())
+            {
+                List<DirectoryInfo> folders_in_sub_directory = new List<DirectoryInfo>();
+
+                //try to get all sub directory
+                try
+                {
+                    DirectoryInfo[] subdirectories = directory.GetDirectories();
+
+                    //check if current subdirectory has more subdirectories
+                    folders_in_sub_directory = get_all_directories(sub_directory);
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return all_directories;
+                }
+
+                //add sub directories
+                all_directories.Add(sub_directory);
+
+                //if there are some sub directories inside current sub_directory then add them to all_directories
+                if (folders_in_sub_directory.Count > 0)
+                    all_directories.AddRange(folders_in_sub_directory);
+            }
+
+            return all_directories;
+        }
+
+        private void search_files(DirectoryInfo directory,string filename)
+        {
+            //here it is stored files that contains filename
+            List<string> files = new List<string>();
+
+            FileInfo[] all_files = directory.GetFiles();
+            foreach (FileInfo file in all_files)
+            {
+                if (file.Name.Contains(filename))
+                {
+                    files_ListBox.Items.Add(file.Name);
+                    file_list.Add(file.FullName);
+                }
+            }
+
+        }
+
+        
+        //////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////
+        /*                                 EVENTS                                             */
+        private void open_folder_browserBtn_Click(object sender, EventArgs e)
+        {
+            //open folder browser. It allows users to select the path of folder that they want to.
+            folderBrowserDialog1.ShowDialog();
+            directoryCombobox.Text = "";
+            //update the path
+            directoryCombobox.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void file_nameTxt_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                searchBtn.PerformClick();
+        }
+
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+
+            //try to get the directory and subdirectory from the path given by the user
+            string path = directoryCombobox.Text;
+            DirectoryInfo directory;
+            List<DirectoryInfo> all_directories = new List<DirectoryInfo>();
+            //reset progress bar to 0
+            progressBar1.Value = 0;
+
+            //show the progress (not accurate, it is used to let user know that the app is not frozen, it is searching all sub directories)
+            searching_progressLbl.Text = "Searching folders";
+            progressBar1.Value = 50;
+            
+            //close the next message autotically after 1s
+            Press_esc_timer.Enabled = true;
+            Press_esc_timer.Start();
+
+            //pop up the message to tell the user that the searching is started
+            MessageBox.Show("Started to searching, please wait", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            
+
+            try
+            {
+                //get the directory info
+                directory = new DirectoryInfo(path);
+                //get all sub directories
+                all_directories = get_all_directories(directory);
+
+                //all sub directories found
+                progressBar1.Value = 100;
+            }
+            catch (Exception error)
+            {
+                //reset progress bar to 0
+                progressBar1.Value = 0;
+                MessageBox.Show(error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //reset to the progress to default (0)
+            searching_progressLbl.Text = "Searching...0%";
+            progressBar1.Value = 0;
+
+            //clear listbox
+            files_ListBox.Items.Clear();
+
+            //clear file_list
+            file_list.Clear();
+
+            //Use those variables to show the percentage of searching done
+            int directory_completed = 0;
+            int progress_percentage = directory_completed / all_directories.Count * 100;
+
+            //search files from directory (path given by the user)
+            search_files(directory, file_nameTxt.Text);
+
+            //search all directories found
+            foreach (DirectoryInfo sub in all_directories)
+            {
+                //search
+                search_files(sub, file_nameTxt.Text);
+
+                //searching for sub done, so directory_completed + 1
+                directory_completed++;
+
+                //update the progress
+                progress_percentage = directory_completed * 100 / all_directories.Count ;
+                searching_progressLbl.Text = "Searching..." + progress_percentage + "%";
+                progressBar1.Value = progress_percentage;
+            }
+
+            //searching complete
+            searching_progressLbl.Text = "Done";
+
+            //add the directory path into the combobox as "history"
+            directoryCombobox.Items.Add(directoryCombobox.Text);
+        }
+
+
+        private void files_ListBox_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int index = files_ListBox.IndexFromPoint(e.Location);
+            if (index != ListBox.NoMatches)
+            {
+                System.Diagnostics.Process.Start(file_list[index]);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //save the "history" (items in the combobox in a file) so next time we open the app the history remain
+            //it will cancel the previous file is exists
+            using (StreamWriter file = File.CreateText("history.txt"))
+            {
+                for (int i = 0; i< directoryCombobox.Items.Count; i++)
+                    file.WriteLine(directoryCombobox.Items[i]);
+            }
+         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("history.txt"))
+            {
+                //read the file and add it to combobox items
+                using (StreamReader readtext = new StreamReader("history.txt"))
+                {
+                    while (!readtext.EndOfStream)
+                        directoryCombobox.Items.Add(readtext.ReadLine());
+
+                    if (directoryCombobox.Items.Count > 0 && directoryCombobox.Items[directoryCombobox.Items.Count - 1].ToString() != "Clear")
+                        directoryCombobox.Items.Add("Clear");
+
+                }
+            }
+          
+        }
+
+
+        private void directoryCombobox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selected_item = directoryCombobox.GetItemText(directoryCombobox.SelectedItem);
+
+            if (selected_item == "Clear")
+            {
+                DialogResult result = MessageBox.Show("Are you sure to delete the history?", "Alert", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                    directoryCombobox.Items.Clear();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SendKeys.SendWait("{ESC}");
+
+            Press_esc_timer.Stop();
+            Press_esc_timer.Enabled = false;
+        }
+    }
+}
